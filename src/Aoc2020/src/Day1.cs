ca77daede6580bed5b0633e0666ef51e34ff7895
Carl Åstholm <carl@astholm.se>
2020-12-01 10:13:41 +0100

    Day 1

--- src/Aoc2020/src/Day1.cs
+++ src/Aoc2020/src/Day1.cs
@@ -1,15 +1,52 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
 namespace Aoc2020
 {
     public static class Day1
     {
         public static string Part1(string input)
         {
-            return "Hello";
+            var lines = SplitOnNewline(input)
+                .Select(s => int.Parse(s, NumberStyles.Integer, CultureInfo.InvariantCulture))
+                .ToArray();
+
+            return lines
+                .SelectMany((left, leftOffset) => lines
+                    .Skip(leftOffset)
+                    .SelectMany(right => left + right == 2020
+                        ? new[] { left * right }
+                        : Enumerable.Empty<int>()))
+                .First()
+                .ToString(CultureInfo.InvariantCulture);
         }
 
         public static string Part2(string input)
         {
-            return "World";
+            var lines = SplitOnNewline(input)
+                .Select(s => int.Parse(s, NumberStyles.Integer, CultureInfo.InvariantCulture))
+                .ToArray();
+
+            return lines
+                .SelectMany((left, leftOffset) => lines
+                    .Skip(leftOffset)
+                    .SelectMany((middle, middleOffset) => lines
+                        .Skip(middleOffset)
+                        .SelectMany(right => left + middle + right == 2020
+                            ? new[] { left * middle * right }
+                            : Enumerable.Empty<int>())))
+                .First()
+                .ToString(CultureInfo.InvariantCulture);
+        }
+
+        private static IEnumerable<string> SplitOnNewline(string input)
+        {
+            using var reader = new StringReader(input);
+
+            while (reader.ReadLine() is string line)
+                yield return line;
         }
     }
 }
