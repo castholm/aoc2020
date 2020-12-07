bb5b994e37a441174443a5883a5ba0c0236e1e24
Carl Åstholm <carl@astholm.se>
2020-12-07 11:36:44 +0100

    Clean up Day 7

--- src/Aoc2020/src/Day7.cs
+++ src/Aoc2020/src/Day7.cs
@@ -1,6 +1,5 @@
+using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -8,19 +7,21 @@ namespace Aoc2020
 {
     public static class Day7
     {
+        private record ContaineeInfo(string Container, string Containee, int ContaineeCount);
+
+        private static IEnumerable<ContaineeInfo> AsContaineeInfo(string input)
+            => input.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.Split(" bags contain ", 2))
+            .SelectMany(lr => lr[1].Split(", ")
+                .Select(r => Regex.Match(r, @"^(\d+) (\w+ \w+)"))
+                .Where(m => m.Success)
+                .Select(m => new ContaineeInfo(lr[0], m.Groups[2].Value, int.Parse(m.Groups[1].Value))));
+
         public static string Part1(string input)
         {
-            var containerLookup = SplitOnNewline(input)
-                .Select(s => s.Split(" bags contain ", 2))
-                .SelectMany(lr => lr[1].Split(", ")
-                    .Select(r => Regex.Match(r, "^[0-9]+ ([a-z]+ [a-z]+)"))
-                    .Where(m => m.Success)
-                    .Select(m => (Container: lr[0], Containee: m.Groups[1].Value)))
-                .ToLookup(x => x.Containee);
+            var containerLookup = AsContaineeInfo(input).ToLookup(x => x.Containee);
 
-            return GetContainers("shiny gold")
-                .Count()
-                .ToString(CultureInfo.InvariantCulture);
+            return GetContainers("shiny gold").Count().ToString();
 
             IEnumerable<string> GetContainers(string containee)
                 => containerLookup![containee]
@@ -30,34 +31,14 @@ namespace Aoc2020
 
         public static string Part2(string input)
         {
-            var containeeLookup = SplitOnNewline(input)
-                .Select(s => s.Split(" bags contain ", 2))
-                .SelectMany(lr => lr[1].Split(", ")
-                    .Select(r => Regex.Match(r, "^([0-9]+) ([a-z]+ [a-z]+)"))
-                    .Where(m => m.Success)
-                    .Select(m => (
-                        Container: lr[0],
-                        Containee: m.Groups[2].Value,
-                        ContaineeCount: int.Parse(
-                            m.Groups[1].Value,
-                            NumberStyles.None,
-                            CultureInfo.InvariantCulture))))
-                .ToLookup(x => x.Container);
+            var containeeLookup = AsContaineeInfo(input).ToLookup(x => x.Container);
 
-            return SumContainees("shiny gold").ToString(CultureInfo.InvariantCulture);
+            return GetTotalContaineeCount("shiny gold").ToString();
 
-            int SumContainees(string container)
+            int GetTotalContaineeCount(string container)
                 => containeeLookup![container]
-                .Select(x => (SumContainees(x.Containee) + 1) * x.ContaineeCount)
+                .Select(x => (GetTotalContaineeCount(x.Containee) + 1) * x.ContaineeCount)
                 .Sum();
-        }
-
-        private static IEnumerable<string> SplitOnNewline(string input)
-        {
-            using var reader = new StringReader(input);
-
-            while (reader.ReadLine() is string line)
-                yield return line;
         }
     }
 }
